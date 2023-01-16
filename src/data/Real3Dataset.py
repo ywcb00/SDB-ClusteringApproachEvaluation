7e371eb3d8f2b992478f20aa1807ade91ba59773
gremay <gregor.mayr@student.tugraz.at>
2023-01-16 17:56:46 +0100

    Implemented Real3

--- src/data/Real3Dataset.py
+++ src/data/Real3Dataset.py
@@ -1,25 +1,35 @@
-from data.Dataset import IDataset
+from src.data.Dataset import IDataset
+import geopandas as gpd
+
 
 class Real3Dataset(IDataset):
-    def __init__(self):
+    def __init__(self, path):
         super().__init__()
+        self.path = path
 
     def createTables(self):
         # create the required postgres tables
-        raise NotImplementedError()
+        create_stmt = '''CREATE TABLE IF NOT EXISTS real3(
+                    id SERIAL PRIMARY KEY NOT NULL,
+                    geom GEOMETRY(Point, 26918) NOT NULL,
+                    cid INT
+                );'''
+        self.pgc.execute(create_stmt)
         return
 
     def preprocess(self):
         # data generation and transformation if needed
-        raise NotImplementedError()
+        self.data = gpd.read_file(self.path)
         return
 
     def pushData(self):
         # insert the dataset into the postgres instance
-        raise NotImplementedError()
+        value_str = ", ".join([f'(ST_Point({point.x}, {point.y}))' for point in self.data['geometry']])
+        insert_stmt = f'''INSERT INTO real3 (geom) VALUES {value_str};'''
+        self.pgc.execute(insert_stmt)
         return
 
     def dropTables(self):
         # remove the previously created postgres tables
-        raise NotImplementedError()
-        return
+        drop_stmt = '''DROP TABLE real3;'''
+        self.pgc.execute(drop_stmt)
