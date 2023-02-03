79b89661bda06998d262e6b4a7d14b8d5ed80234
ywcb00 <ywcb00@ywcb.org>
2023-02-03 13:57:41 +0100

    feat(MemoryConsumptionMonitor.py): monitor the memory consumption of the current python process 	NOTE: only the single current python process considered, not supporting multiprocessing

--- src/utils/MemoryConsumptionMonitor.py
+++ src/utils/MemoryConsumptionMonitor.py
@@ -1,6 +1,8 @@
 import threading
 import time
 import numpy as np
+import pandas as pd
+import os
 from PostgresController import PostgresController
 from proc.core import find_processes
 
@@ -8,10 +10,10 @@ from proc.core import find_processes
 class MemoryConsumptionMonitor():
     def __init__(self, interval):
         self.interval = interval
-        self.memoryconsumptions = np.empty(None)
+        self.memoryconsumptions = dict()
         self.pgc = PostgresController()
 
-    def measure(self):
+    def measurePG(self):
         STAT_QUERY = 'SELECT pid FROM pg_stat_activity;'
         pg_pids = self.pgc.executeFetch(STAT_QUERY)
         pg_pids = [pgs[0] for pgs in pg_pids]
@@ -19,11 +21,33 @@ class MemoryConsumptionMonitor():
         pg_memc = np.sum([p.rss for p in pg_procs])
         return pg_memc
 
+    def measurePython(self):
+        pyt_pid = os.getpid()
+        pyt_procs = [pytp for pytp in find_processes() if pytp.pid == pyt_pid]
+        if(len(pyt_procs) != 1):
+            assert "Multiple python processes found"
+        pyt_memc = pyt_procs[0].rss
+        return pyt_memc
+
+    def measure(self):
+        memc = dict()
+        memc["pg_memc"] = [self.measurePG()]
+        memc["pyt_memc"] = [self.measurePython()]
+        return memc
+
+    def mergeMeasurements(self, d1, d2):
+        if (len(d1.keys()) == 0):
+            return d2
+        for key in d1.keys():
+            d1[key].extend(d2[key])
+        return d1
+
     def monitor(self):
         while(not self.terminate):
             time.sleep(self.interval)
             memcons = self.measure()
-            self.memoryconsumptions = np.append(self.memoryconsumptions, memcons)
+            self.memoryconsumptions = self.mergeMeasurements(
+                self.memoryconsumptions, memcons)
 
     def startMonitoring(self):
         self.terminate = False
@@ -33,4 +57,5 @@ class MemoryConsumptionMonitor():
     def stopMonitoring(self):
         self.terminate = True
         self.thread.join()
-        return np.max(self.memoryconsumptions)
+        df_memc = pd.DataFrame(self.memoryconsumptions)
+        return df_memc.max(axis=0)
