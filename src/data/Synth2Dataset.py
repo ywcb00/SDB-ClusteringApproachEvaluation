8b97ebce723daa94eadc24fda20268a164443706
Sigmaeon <sbraito@student.tugraz.at>
2023-02-04 18:50:29 +0100

    Added Parameter `size` to Synth2 dataset

--- src/data/Synth2Dataset.py
+++ src/data/Synth2Dataset.py
@@ -7,15 +7,16 @@ class Synth2Dataset(IDataset):
     class ClusterConfig:
         mean: np.ndarray[(1,3), np.float64]
         cov: np.ndarray[(3,3), np.float64]
-        size: int
+        prop: float # proprtion of the total size
         m: float
 
-    def _make_cluster(self, config: ClusterConfig) -> np.array:
-        cluster_coords = self._rng.multivariate_normal(config.mean, config.cov, config.size)
+    def _make_cluster(self, config: ClusterConfig, size: int) -> np.array:
+        clust_size = int(size*config.prop)
+        cluster_coords = self._rng.multivariate_normal(config.mean, config.cov, clust_size)
         if self._discrete_m:
-            return np.hstack([cluster_coords, np.vstack([config.m]*config.size)])
+            return np.hstack([cluster_coords, np.vstack([config.m]*clust_size)])
         else:
-            return np.hstack([cluster_coords, np.vstack(self._rng.normal(config.m, self._var_m, config.size))])
+            return np.hstack([cluster_coords, np.vstack(self._rng.normal(config.m, self._var_m, clust_size))])
 
     def _to_spd(self, a: np.ndarray[(1,9)]) -> np.ndarray[(3,3)]:
         a = a.reshape((3,3))
@@ -43,13 +44,13 @@ class Synth2Dataset(IDataset):
         self._var_m = 0.3
         self._latlon_transform = True
         self._base_latlon = np.array([47.076747, 15.437288])
-        self._offset_factor = 100 # offset for in meters for distance of 1
+        self._offset_factor = 500 # offset for in meters for distance of 1
         self._cluster_configs = [
-            self.ClusterConfig(np.array([6,6,6]), np.identity(3)*0.5, 1000, 1),
-            self.ClusterConfig(np.array([1,2,3]), np.identity(3)*1, 5000, 2),
-            self.ClusterConfig(np.array([10,0,0]), self._to_spd(self._rng.random(9)), 200, 3),
-            self.ClusterConfig(np.array([0,6,9]), self._to_spd(self._rng.normal(2, 0.5, 9)), 2000, 4),
-            self.ClusterConfig(np.array([6,1,9]), np.identity(3)*np.array([1,2,3]), 700, 5)
+            self.ClusterConfig(np.array([6,6,6]), np.identity(3)*0.5, 30/100, 1),
+            self.ClusterConfig(np.array([1,2,3]), np.identity(3)*1, 10/100, 2),
+            self.ClusterConfig(np.array([10,0,0]), self._to_spd(self._rng.random(9)), 5/100, 3),
+            self.ClusterConfig(np.array([0,6,9]), self._to_spd(self._rng.normal(2, 0.5, 9)), 25/100, 4),
+            self.ClusterConfig(np.array([6,1,9]), np.identity(3)*np.array([1,2,3]), 30/100, 5)
         ]
 
     def createTables(self):
@@ -61,8 +62,9 @@ class Synth2Dataset(IDataset):
         self.pgc.execute(create_stmt)
 
     def preprocess(self, n=10000):
+        assert(int(sum([c.prop for c in self._cluster_configs])) == 1)
         # data generation and transformation if needed
-        self._data = np.concatenate([self._make_cluster(config) for config in self._cluster_configs])
+        self._data = np.concatenate([self._make_cluster(config, n) for config in self._cluster_configs])
         if self._latlon_transform:
             self._data = self._approx_transform_to_latlon(self._data)
 
